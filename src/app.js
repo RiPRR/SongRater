39b18d77f88a15efc95fae00044e968394115e4b
RiPRR <rpr288@nyu.edu>
2019-05-09 17:45:33 -0400

    update

--- src/app.js
+++ src/app.js
@@ -21,6 +21,7 @@ app.use(passport.session())
 app.use(cookieParser())
 app.use(express.static('public'))
 app.use(express.static(__dirname + '/public'))
+app.set('views', path.join(__dirname, '/src/views'));
 app.set("view-engine","hbs")
 
 const routes = require("./routes/index")(passport)
